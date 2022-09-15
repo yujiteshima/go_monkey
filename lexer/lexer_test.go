25fdb0e3416dee581178f342776e16dcc6208d1e
Yuji Teshima <yuji.teshima@gmail.com>
2022-09-15 22:53:39 +0900

    add lexer

---
+++ lexer/lexer_test.go
@@ -0,0 +1,35 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/yujiteshima/go_monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - token wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
